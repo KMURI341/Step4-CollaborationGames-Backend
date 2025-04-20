bac830073c349cad5748a173c781a17fd06f183f
KMURAI <m.m.k.j.izum.h.k.m.2126@gmail.com>
2025-04-20 23:17:45 +0900

    Fix CORS settings

--- app/main.py
+++ app/main.py
@@ -48,7 +48,7 @@ app = FastAPI(
 # CORSミドルウェアの設定
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # 開発中は全てのオリジンを許可
+    allow_origins=["https://tech0-gen-8-step4-bic-frontend.azurewebsites.net", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
